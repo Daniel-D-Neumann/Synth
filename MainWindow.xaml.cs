83b76789a328cd4044820a5de2349e5125cf827e
Daniel Neumann <115144777+Daniel-D-Neumann@users.noreply.github.com>
2024-04-25 11:36:11 +0100

    Basic sound gen

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Media;
+
+namespace Synth
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private const int SAMPLE_RATE = 44100;
+        private const short BITS_PER_SAMPLE = 16;
+        private const int NUM_OF_CHANNELS = 1;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            short[] wave = new short[SAMPLE_RATE];
+            byte[] binaryWave = new byte[SAMPLE_RATE*sizeof(short)];
+            float frequency = 220f;
+
+            //sine wave
+            for (int i = 0; i < SAMPLE_RATE; i++)
+            {
+                wave[i] = Convert.ToInt16(short.MaxValue * Math.Sin(((Math.PI * 2 * frequency) / SAMPLE_RATE) * i));
+            }
+            Buffer.BlockCopy(wave,0, binaryWave, 0,wave.Length*sizeof(short));
+            using (MemoryStream memoryStream = new MemoryStream())
+            using (BinaryWriter binaryWriter = new BinaryWriter(memoryStream)) 
+            {
+                short blockAlign = BITS_PER_SAMPLE / 8;
+                int subChunk2Size = SAMPLE_RATE * NUM_OF_CHANNELS * blockAlign;
+                binaryWriter.Write(new[] { 'R', 'I', 'F', 'F' });
+                binaryWriter.Write(36+subChunk2Size);
+                binaryWriter.Write(new[] { 'W', 'A', 'V', 'E','f','m','t',' ' });
+                binaryWriter.Write(16);
+                binaryWriter.Write((short)1);
+                binaryWriter.Write((short)1);
+                binaryWriter.Write(SAMPLE_RATE);
+                binaryWriter.Write(SAMPLE_RATE*blockAlign);
+                binaryWriter.Write(blockAlign);
+                binaryWriter.Write(BITS_PER_SAMPLE);
+                binaryWriter.Write(new[] { 'd', 'a', 't', 'a' });
+                binaryWriter.Write(subChunk2Size);
+                binaryWriter.Write(binaryWave);
+                memoryStream.Position = 0;
+                new SoundPlayer(memoryStream).Play();
+            }
+        }
+    }
+}
