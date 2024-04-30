b31324d2ddf346e6a7c7d82db29dfe4564f777e6
Daniel Neumann <115144777+Daniel-D-Neumann@users.noreply.github.com>
2024-04-30 17:17:07 +0100

    Created GenNote function
    This takes a gain, frequency and time, returning the associated generated audio with these values.
    
    Defining frequencies for common notes would be a good idea.

--- NAudioSynth/MainWindow.xaml.cs
+++ NAudioSynth/MainWindow.xaml.cs
@@ -25,7 +25,7 @@ namespace NAudioSynth
     /// </summary>
     public partial class MainWindow : Window
     {
-
+        float D3 = 150;
         
 
         public MainWindow()
@@ -37,11 +37,20 @@ namespace NAudioSynth
         {
             return new SignalGenerator()
             {
-                Gain = 0.2,
+                Gain = 30,
                 Frequency = 150,
                 Type = SignalGeneratorType.Sin
             }
             .Take(TimeSpan.FromSeconds(.2));
+        }
+
+        private ISampleProvider GenNote(float gain, float frequency, float time) 
+        {
+            return new SignalGenerator()
+            {
+                Gain = gain,
+                Frequency = frequency,
+            }.Take(TimeSpan.FromSeconds(.2));
         }
 
         private ISampleProvider GenSilence()
@@ -49,12 +58,13 @@ namespace NAudioSynth
             return new SilenceProvider(new WaveFormat()).ToSampleProvider().Take(TimeSpan.FromSeconds(.2));
         }
 
-        private ISampleProvider CreateMetronome(int length, Func<ISampleProvider> note)
+        private ISampleProvider CreateMetronome(int length, float gain, float frequency, float time)
         {
             ISampleProvider[] metronomeConcat = new ISampleProvider[length*2];
+            //ISampleProvider myNote = GenD3();            
             for (int i = 0; i < length; i++)
             {
-                metronomeConcat[i*2] = note();
+                metronomeConcat[i*2] = GenNote(gain,frequency,time);
                 //OffsetSampleProvider
                 metronomeConcat[(i*2)+1] = GenSilence();
             }
@@ -65,15 +75,15 @@ namespace NAudioSynth
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            //var DLow = new SignalGenerator()
-            //{
-            //    Gain = 0.2,
-            //    Frequency = 150,
-            //    Type = SignalGeneratorType.Sin
+            var DLow = new SignalGenerator()
+            {
+                Gain = 0.2,
+                Frequency = 150,
+                Type = SignalGeneratorType.Sin
 
-            //}.Take(TimeSpan.FromSeconds(2));
+            }.Take(TimeSpan.FromSeconds(.2));
 
-            //var silence = new SilenceProvider(new WaveFormat()).ToSampleProvider().Take(TimeSpan.FromSeconds(2));
+            var silence = new SilenceProvider(new WaveFormat()).ToSampleProvider().Take(TimeSpan.FromSeconds(.2));
             //var A = new SignalGenerator()
             //{
             //    Gain = 0.2,
@@ -106,7 +116,9 @@ namespace NAudioSynth
 
             //var concat = DLow.FollowedBy(silence).FollowedBy(DLow);
 
-            var concat = CreateMetronome(5, GenD3);
+            var concat = CreateMetronome(5, 0.2f,D3,0.2f);
+        //https://stackoverflow.com/questions/65726394/playing-waveform-with-naudio-lower-for-each-turn
+        //https://github.com/naudio/NAudio/issues/373
 
             using (var wo = new WaveOutEvent())
             {
